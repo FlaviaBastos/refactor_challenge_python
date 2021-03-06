fe1aa6959ad08a28344be16b3bb5e3050666a27c
Flavia Bastos <flasanches@gmail.com>
2021-03-06 14:48:15 -0500

    Refactor research

--- src/research.py
+++ src/research.py
@@ -5,5 +5,5 @@ class Research:
     def __init__(self, relationships):
         relations = relationships.relations
         for r in relations:
-            if r[0].name == "Bob" and r[1] == Relationship.BOSS:
-                print(f"Bob has an employee {r[2].name}.")
+            if r[1] == Relationship.BOSS:
+                print(f"{r[0].name} has an employee {r[2].name}.")
